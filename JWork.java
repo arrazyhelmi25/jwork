edde1235b64a0bb07c5a78f5e411c0cec9196d18
Helmi <helmi,arrazy@ui.ac.id>
2021-03-04 18:15:05 +0700

    modul 1 - CS

---
+++ JWork.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class JWork here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JWork
+{
+    // instance variables - replace the example below with your own
+    public static void main (String [] args) 
+    {
+        
+    }
+}
